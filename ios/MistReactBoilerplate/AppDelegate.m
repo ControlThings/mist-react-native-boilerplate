1ca283a9ef3e03dd6541a8f0ca9b51256a545941
Jan Nyman <jan.nyman@controlthings.fi>
2019-05-02 15:15:16 +0300

    Enabling debug vs. release standalone bundle on iOS

--- ios/MistReactBoilerplate/AppDelegate.m
+++ ios/MistReactBoilerplate/AppDelegate.m
@@ -16,7 +16,11 @@
 {
   NSURL *jsCodeLocation;
 
+#if DEBUG == 1
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+#else
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"MistReactBoilerplate"
